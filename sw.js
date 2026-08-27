547a223bd08640da36efb3cde37cc73de368bd08
yunusemreka <kayayunusemre4@gmail.com>
2026-08-27 15:13:32 +0300

    feat: PWA service worker destegi eklendi

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
